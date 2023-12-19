75a58b682ff6077780f167217ae437c31a735121
Erika Ampudia-Vales <emavales2@gmail.com>
2023-12-19 01:46:35 -0500

    finished project, added config/pagination.php

--- Maisonneuve_2395411/resources/views/etudiant/show.blade.php
+++ Maisonneuve_2395411/resources/views/etudiant/show.blade.php
@@ -1,55 +1,51 @@
 @extends('layouts.layout')
 @section('content')
 
-    <div class="row">
-        <div class="col-12 pt-2">
-            <a href="{{ route('etudiant.index')}}" class="btn btn-outline-primary">Retourner</a>
-            <h4 class="display-6 mt-2">
-                {{ $etudiant->name }}
-            </h4>
-            <hr>
-            
-            <p><strong>Email :</strong>{{ $etudiant->email }}</p>
-            <p><strong>No. Téléphone:</strong>{{ $etudiant->phone }}</p>
-            <p><strong>Adresse :</strong>{{ $etudiant->address }}</p>
-            <p><strong>Ville :</strong>{{ $etudiant->etudiantHasVille->name }}</p>
-            <p><strong>Date de Naissance :</strong>{{ $etudiant->birthday }}</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-6">
-            <a href="{{ route('etudiant.edit', $etudiant->id)}}" class="btn btn-primary">Modifier</a>
-        </div>
-        <div class="col-6">
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
-                Effacer
-                </button>
-        </div>
-    </div>
+  <div class="col-md-6 d-flex flex-column align-items-center border border-dark-subtle p-5 rounded-5 mt-5 mw-40em m-auto">      
+    <header class="m-5 mt-0">
+        <h2 class="text-center fw-bold fs-3em text-primary">{{ $etudiant->name }}</h2>
+    </header>
+    <section>
+      <p><strong>Email :</strong>{{ $etudiant->email }}</p>
+      <p><strong>No. Téléphone:</strong>{{ $etudiant->phone }}</p>
+      <p><strong>Adresse :</strong>{{ $etudiant->address }}</p>
+      <p><strong>Ville :</strong>{{ $etudiant->etudiantHasVille->name }}</p>
+      <p><strong>Date de Naissance :</strong>{{ $etudiant->birthday }}</p>
+    </section>
+    
+    <aside class="d-flex flex-row gap-5 pt-4">
+      <a href="{{ route('etudiant.edit', $etudiant->id)}}" class="btn btn-primary">Modifier</a>
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#deleteModal">
+        Effacer
+      </button>        
+    </aside>
+  </div>
+  
 
+  <!-- Modal -->
+  <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
 
+      <article class="modal-content">
+        <header class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Effacer la donnée</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </header>
 
-<!-- Modal -->
-<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Effacer la donnée</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-       Etes-vous sûr de efffacer la donnée?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Non</button>
-        <form method="post">
-            @csrf
-            @method('delete')
-            <input type="submit" value="Effacer" class="btn btn-danger">
-        </form>
-      </div>
+        <p class="modal-body">
+        Etes-vous sûr de efffacer la donnée?
+        </p>
+
+        <footer class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Non</button>
+          <form method="post">
+              @csrf
+              @method('delete')
+              <input type="submit" value="Effacer" class="btn btn-danger">
+          </form>
+        </footer>
+
+      </article>
     </div>
   </div>
-</div>
-@endsection
+  @endsection
--- Maisonneuve_2395411/resources/views/home.blade.php
+++ Maisonneuve_2395411/resources/views/home.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.layout')
 @section('content')
 
-<header class="pt-10">
-    <h1 class="text-5xl text-center font-semibold p-8 pt-25">BIENVENU/E</h1>
+<header class="m-5">
+    <h1 class="text-center pt-5 fw-bold fs-4em text-primary">Bienvenu|e !</h1>
 </header>
                
 <div>
-    <p class="text-xl text-center">Servez-vous de la navigation en haut pour parcourir la liste d'étudiant/es ou en créer une nouvelle donnée</p>
+    <p class="text-center text-secondary">Servez-vous de la navigation en haut pour parcourir la liste d'étudiant|es ou pour créer un nouvel record</p>
 </div>           
 
 @endsection
